0c66bedf273ea2148d6e4ede34d33fa22c0ced8e
RedaBerkouch <reda.berkouch@sword-group.com>
2025-04-06 03:03:58 +0200

    add read me

--- frontend/src/app/service/user.service.ts
+++ frontend/src/app/service/user.service.ts
@@ -8,8 +8,8 @@ import { Key } from '../enum/key.enum';
 
 @Injectable()
 export class UserService {
-  private readonly server: string = 'http://vps-27d6c134.vps.ovh.net:8080';
-  //private readonly server: string = 'http://localhost:8080';
+  //private readonly server: string = 'http://vps-27d6c134.vps.ovh.net:8080';
+  private readonly server: string = 'http://localhost:8080';
   private jwtHelper = new JwtHelperService();
 
   constructor(private http: HttpClient) { }
