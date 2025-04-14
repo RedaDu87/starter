47c90b7db5fb96b7797759bead74afae50f63c08
RedaBerkouch <reda.berkouch@sword-group.com>
2025-04-14 06:19:04 +0200

    add read me

--- backend/src/main/java/io/getarrays/securecapita/constant/Constants.java
+++ backend/src/main/java/io/getarrays/securecapita/constant/Constants.java
@@ -12,7 +12,15 @@ public class Constants {
             "/user/login/**", "/user/verify/code/**", "/user/register/**", "/user/resetpassword/**", "/user/verify/account/**",
             "/user/refresh/token/**", "/user/image/**", "/user/new/password/**" ,"/api/quran/**", "/api/hadith/**"};
     public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String[] PUBLIC_ROUTES = { "/user/new/password", "/user/login", "/user/verify/code", "/user/register", "/user/refresh/token", "/user/image" };
+    public static final String[] PUBLIC_ROUTES = {
+            "/user/new/password", "/user/login",
+            "/user/verify/code",
+            "/user/register",
+            "/user/refresh/token",
+            "/user/image",
+            "/api/quran/**",
+            "/api/hadith/**"
+    };
     public static final String HTTP_OPTIONS_METHOD = "OPTIONS";
     public static final String AUTHORITIES = "authorities";
     public static final String GET_ARRAYS_LLC = "GET_ARRAYS_LLC";
