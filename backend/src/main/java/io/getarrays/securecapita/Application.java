6dd803bc136f12efbb97d25b4731dad517ecab86
RedaBerkouch <reda.berkouch@sword-group.com>
2025-04-27 00:39:23 +0200

    add read me

--- backend/src/main/java/io/getarrays/securecapita/Application.java
+++ backend/src/main/java/io/getarrays/securecapita/Application.java
@@ -11,11 +11,19 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.List;
 import java.util.Arrays;
 
-
+/**
+ * @author Junior RT
+ * @version 1.0
+ * @license Get Arrays, LLC (https://getarrays.io)
+ * @since 8/22/2022
+ */
 
 @SpringBootApplication//(exclude = { SecurityAutoConfiguration.class })
 public class Application {
 
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 
 }
