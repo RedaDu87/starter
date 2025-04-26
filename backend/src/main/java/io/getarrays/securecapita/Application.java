d14e6cb01cfc4cc314ed70bd39aab5a4d131dcc4
RedaBerkouch <reda.berkouch@sword-group.com>
2025-04-27 00:20:59 +0200

    add read me

--- backend/src/main/java/io/getarrays/securecapita/Application.java
+++ backend/src/main/java/io/getarrays/securecapita/Application.java
@@ -11,48 +11,11 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.List;
 import java.util.Arrays;
 
-/**
- * @author Junior RT
- * @version 1.0
- * @license Get Arrays, LLC (https://getarrays.io)
- * @since 8/22/2022
- */
+
 
 @SpringBootApplication//(exclude = { SecurityAutoConfiguration.class })
 public class Application {
-	private static final int STRENGTH = 12;
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(STRENGTH);
-	}
-
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-
-		config.setAllowCredentials(true);
-		config.setAllowedOrigins(List.of(
-				"http://vps-27d6c134.vps.ovh.net:4200",
-				"http://vps-27d6c134.vps.ovh.net:8080",
-				"http://vps-27d6c134.vps.ovh.net:3000",
-				"http://coran-hadith.fr",
-				"http://api.coran-hadith.fr"
-		));
-		config.setAllowedHeaders(List.of(
-				"Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
-				"X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"
-		));
-		config.setExposedHeaders(List.of(
-				"Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "File-Name"
-		));
-		config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
-	}
 
 
 }
--- backend/src/main/java/io/getarrays/securecapita/configuration/SecurityConfig.java
+++ backend/src/main/java/io/getarrays/securecapita/configuration/SecurityConfig.java
@@ -16,6 +16,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 import static io.getarrays.securecapita.constant.Constants.PUBLIC_URLS;
 import static org.springframework.http.HttpMethod.DELETE;
@@ -46,14 +51,18 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable()).cors(withDefaults());
         http.sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
         http.authorizeHttpRequests(request -> request.requestMatchers(PUBLIC_URLS).permitAll());
-        http.authorizeHttpRequests(request -> request.requestMatchers(OPTIONS).permitAll()); // Not needed
+        http.authorizeHttpRequests(request -> request.requestMatchers(OPTIONS).permitAll());
         http.authorizeHttpRequests(request -> request.requestMatchers(DELETE, "/user/delete/**").hasAnyAuthority("DELETE:USER"));
         http.authorizeHttpRequests(request -> request.requestMatchers(DELETE, "/customer/delete/**").hasAnyAuthority("DELETE:CUSTOMER"));
         http.exceptionHandling(exception -> exception.accessDeniedHandler(customAccessDeniedHandler).authenticationEntryPoint(customAuthenticationEntryPoint));
         http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+
+        http.addFilterBefore(corsFilter(), CustomAuthorizationFilter.class); // <== AJOUTE CECI
         http.addFilterBefore(customAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
+
 
     @Bean
     public AuthenticationManager authenticationManager() {
@@ -61,6 +70,40 @@ public class SecurityConfig {
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(encoder);
         return new ProviderManager(authProvider);
+    }
+
+    private static final int STRENGTH = 12;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(STRENGTH);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of(
+                "http://vps-27d6c134.vps.ovh.net:4200",
+                "http://vps-27d6c134.vps.ovh.net:8080",
+                "http://vps-27d6c134.vps.ovh.net:3000",
+                "http://coran-hadith.fr",
+                "http://api.coran-hadith.fr"
+        ));
+        config.setAllowedHeaders(List.of(
+                "Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+                "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"
+        ));
+        config.setExposedHeaders(List.of(
+                "Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "File-Name"
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
     /* Documentations
