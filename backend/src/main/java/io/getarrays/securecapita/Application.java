1425a778887ddbcd2bc9512a42284597ac074c96
RedaBerkouch <reda.berkouch@sword-group.com>
2025-04-27 19:19:40 +0200

    add read me

--- backend/src/main/java/io/getarrays/securecapita/Application.java
+++ backend/src/main/java/io/getarrays/securecapita/Application.java
@@ -20,10 +20,37 @@ import java.util.Arrays;
 
 @SpringBootApplication//(exclude = { SecurityAutoConfiguration.class })
 public class Application {
+    private static final int STRENGTH = 12;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(STRENGTH);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(List.of(
+                "http://vps-27d6c134.vps.ovh.net:*",
+                "http://coran-hadith.fr",
+                "http://api.coran-hadith.fr"
+        ));
+        config.setAllowedHeaders(List.of(
+                "Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+                "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"
+        ));
+        config.setExposedHeaders(List.of(
+                "Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "File-Name"
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 
 
 }
