eb9e271653d85e275b6a1a7900c90b51f2fbc23c
RedaBerkouch <reda.berkouch@sword-group.com>
2025-04-27 20:43:42 +0200

    add read me

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'http://api.coran-hadith.fr/',
+  apiUrl: 'http://api.coran-hadith.fr',
   coranUrl: 'http://api.coran-hadith.fr/api/quran',
   hadtihUrl: 'http://api.coran-hadith.fr/api/hadith'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://api.coran-hadith.fr/',
+  apiUrl: 'http://api.coran-hadith.fr',
   coranUrl: 'http://api.coran-hadith.fr/api/quran',
   hadtihUrl: 'http://api.coran-hadith.fr/api/hadith'
 };
